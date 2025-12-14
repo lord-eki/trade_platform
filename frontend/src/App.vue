e8f9eeb008517fdd4e07db9ce3565c17ba1bb8de
brian lodeki <lodekib@gmail.com>
2025-12-14 22:03:57 +0300

    Set up main app entry point

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import { onMounted } from 'vue';
+import { useAuthStore } from './stores/auth';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.checkAuth();
+});
+</script>
+
+<template>
+  <RouterView />
+</template>
